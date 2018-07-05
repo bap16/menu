456bf16626ad17c0d89bfc9f441ce01befff78e2
bajzat.attila <bajzat.attila@virgo.hu>
2018-07-05 15:57:46 +0200

    EpicDriver beta

--- src/Home/PortalBundle/Model/MenuInterface/RestaurantMenuInterface.php
+++ src/Home/PortalBundle/Model/MenuInterface/RestaurantMenuInterface.php
@@ -6,15 +6,8 @@ interface RestaurantMenuInterface {
 
     public function setRestaurantId();
 
-    public function getRestaurantId();
-
     public function setDay();
-
-    public function getDay();
 
     public function setText();
 
-    public function getText();
-
-    public function getMenuId();
 }
